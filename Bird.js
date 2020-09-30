89f1515582198441d91e37fcb1954f4f4a8c6256
unknown <stevebutti07@gmail.com>
2020-09-29 20:47:16 -0400

    aa

--- Bird.js
+++ Bird.js
@@ -2,12 +2,20 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smoke = loadImage("sprites/smoke.png");
+    this.array = [];
   }
 
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
-
+    if(this.body.velocity.x>10 && this.body.position.x>250){
+    var position = [this.body.position.x,this.body.position.y];
+    this.array.push(position);
+    }
+    for(var i = 0; i<this.array.length;i++){
+      image(this.smoke,this.array[i][0],this.array[i][1])
+    }
     super.display();
   }
 }
